93289cad970184b907ab6949afa27bd07fba4efd
Deise Correia <deisecorreia34@gmail.com>
2021-06-23 08:42:20 -0300

    teste

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -11,7 +11,7 @@ use Symfony\Component\DomCrawler\Crawler;
 $client = new Client(['base_uri' => 'https://www.alura.com.br']);
 $crawler = new Crawler();
 
-$buscador = new Buscador($client, $crawler);
+//$buscador = new Buscador($client, $crawler);
 
 $cursos = $buscador->buscar('/cursos-online-programacao/php');
 echo "<pre>";
